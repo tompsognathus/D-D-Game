8fffa768d0aa58679bdab125dca8fb2689323c94
tompsognathus <tompsognathus@gmail.com>
2023-06-22 01:11:15 +0100

    added basic HUD widget blueprint

---
+++ Source/CharSheet/AdventurerHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AdventurerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARSHEET_API AAdventurerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
